3caadb6f87d96e9a4e6d2cba553ecaa0c02c929d
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-08-03 00:39:49 -0700

    chore(App.js): import ShowPostCC component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import PostChangeByCDU from "./components/PostChangeByCDU";
 import GetPostInCDM from "./components/GetPostInCDM"
 import ShowPostFC from "./components/ShowPostFC";
+import ShowPostCC from "./components/ShowPostCC";
 const App = ()=> {
   return (
     <div >
       <PostChangeByCDU/>
       <GetPostInCDM/>
       <ShowPostFC/>
+      <ShowPostCC/>
     </div>
   );
 }
