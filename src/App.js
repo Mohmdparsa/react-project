a2e81aef51855dd7158ed4e2a518796d059dda37
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-08-03 03:08:34 -0700

    chore(App.js): import ShowPostWithComment component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import PostChangeByCDU from "./components/PostChangeByCDU";
 import GetPostInCDM from "./components/GetPostInCDM"
 import ShowPostFC from "./components/ShowPostFC";
 import ShowPostCC from "./components/ShowPostCC";
+import ShowPostWithComment from "./components/ShowPostWithComment";
 const App = ()=> {
   return (
     <div >
       <PostChangeByCDU/>
       <GetPostInCDM/>
+      <ShowPostWithComment/>
       <ShowPostFC/>
       <ShowPostCC/>
     </div>
